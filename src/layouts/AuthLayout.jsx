37ab1902a9db6beeb6f14e2d4c887a75d667e940
User <user@Users-MacBook-Pro-4.local>
2025-10-03 15:03:38 +0530

    some fixes

--- src/layouts/AuthLayout.jsx
+++ src/layouts/AuthLayout.jsx
@@ -8,25 +8,53 @@ export default function AuthLayout({ title, subtitle, children, contentClassName
       <div
         className="auth-left"
         style={{
+          position: 'relative',
           backgroundImage: `url(${blueBg})`,
           backgroundSize: 'cover',
           backgroundPosition: 'center',
         }}
       >
-        <img
-          src={mask}
-          alt="mask"
+        <div
           style={{
             position: 'absolute',
-            left: '50%',
-            top: '35%',
-            transform: 'translate(-50%, -50%)',
+            top: '5vw',
+            left: 0,
             width: '100%',
-            objectFit: 'contain',
-            pointerEvents: 'none',
+            height: '50%', // top half
           }}
-        />
-        <div className="mark">Cwcnfp</div>
+        >
+          <img
+      src={mask}
+      alt="mask"
+      style={{
+        width: '100%',
+        height: '100%',
+        // objectFit: 'contain', // image scales to fit inside child div
+        pointerEvents: 'none',
+      }}
+    />
+   <span
+    style={{
+      position: 'absolute',
+      bottom: '1.25vw',  // changed from 1.25rem to vw
+      right: '1.25vw',   // changed from 1.25rem to vw
+      fontFamily: 'Poppins',
+      fontWeight: 600,
+      fontStyle: 'normal',
+      fontSize: '2.5vw',
+      lineHeight: '125%',
+      letterSpacing: '0px',
+      textAlign: 'center',
+      color: '#fff',
+  }}
+  
+    >
+      Cwcnfp
+    </span>
+          {/* Content of top half div */}
+        </div>
+        
+        
       </div>
       <div className="auth-right">
         <div className={`panel${contentClassName ? ' ' + contentClassName : ''}`}>
